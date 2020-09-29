fdbc875e2c979059473553cc1e71f8174e8e4288
Riley Park <riley.park@meino.net>
2020-09-29 06:15:25 -0700

    some more work

--- indra-common/src/main/kotlin/net/kyori/indra/IndraPlugin.kt
+++ indra-common/src/main/kotlin/net/kyori/indra/IndraPlugin.kt
@@ -3,12 +3,46 @@ package net.kyori.indra
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaLibraryPlugin
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.api.tasks.javadoc.Javadoc
+import org.gradle.api.tasks.testing.Test
+import org.gradle.external.javadoc.StandardJavadocDocletOptions
+import org.gradle.kotlin.dsl.withType
+import java.nio.charset.StandardCharsets
 
 class IndraPlugin : Plugin<Project> {
   override fun apply(project: Project) {
     with(project) {
       plugins.apply {
         apply(JavaLibraryPlugin::class.java)
+      }
+
+      afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+          it.options.apply {
+            encoding = StandardCharsets.UTF_8.name()
+            compilerArgs.addAll(
+              listOf(
+                "-parameters",
+                "-Xlint:all"
+              )
+            )
+          }
+        }
+
+        tasks.withType<Javadoc>().configureEach {
+          with(it.options) {
+            encoding = StandardCharsets.UTF_8.name()
+
+            if(this is StandardJavadocDocletOptions) {
+              charSet = StandardCharsets.UTF_8.name()
+            }
+          }
+        }
+
+        tasks.withType<Test>().configureEach {
+          it.useJUnitPlatform()
+        }
       }
     }
   }
