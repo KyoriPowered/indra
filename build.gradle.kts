ea1061b4f53e75966f258b32ca5027a76a4af856
zml <zml@stellardrift.ca>
2021-09-12 15:48:28 -0700

    release: Switch target to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "net.kyori"
-version = "2.0.7-SNAPSHOT"
+version = "2.1.0-SNAPSHOT"
 description = "KyoriPowered organizational build standards and utilities"
 
 allprojects {
