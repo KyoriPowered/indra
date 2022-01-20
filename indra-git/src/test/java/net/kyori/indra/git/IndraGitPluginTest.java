c410a408181b6385c9baf73e5ff4ca11c7eb6e24
zml <zml@stellardrift.ca>
2022-01-19 20:44:42 -0800

    indra-git: Add test coverage

--- indra-git/src/test/java/net/kyori/indra/git/IndraGitPluginTest.java
+++ indra-git/src/test/java/net/kyori/indra/git/IndraGitPluginTest.java
@@ -23,16 +23,240 @@
  */
 package net.kyori.indra.git;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import net.kyori.indra.test.IndraTesting;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.gradle.api.Project;
-import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IndraGitPluginTest {
   private static final String PLUGIN = "net.kyori.indra.git";
+  private static final String DEFAULT_BRANCH = "trunk";
+  private static final PersonIdent COMMITTER = new PersonIdent("CI", "noreply@kyori.net");
+
+  @TempDir
+  private Path projectDir;
 
   @Test
   void testPluginSimplyApplies() {
-    final Project project = ProjectBuilder.builder().build();
+    final Project project = IndraTesting.project();
     project.getPluginManager().apply(PLUGIN);
+  }
+
+  @Test
+  void testNoRepositoryDetected() {
+    final IndraGitExtension extension = this.createExtension();
+    assertFalse(extension.isPresent());
+  }
+
+  @Test
+  void testRepositoryDetected() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+    assertTrue(extension.isPresent());
+  }
+
+  @Test
+  void testRepositoryDetectedOnSubprojects() throws IOException, GitAPIException {
+    initRepo(this.projectDir);
+    final Project base = this.createProject();
+    final Project subproject = IndraTesting.project(b -> b.withParent(base));
+    subproject.getPlugins().apply(PLUGIN);
+
+    assertTrue(subproject.getExtensions().getByType(IndraGitExtension.class).isPresent());
+  }
+
+  @Test
+  void testRepositoryIsDetectedWhenOnlyAppliedToSubprojects() throws IOException, GitAPIException {
+    initRepo(this.projectDir);
+    final Project base = IndraTesting.project(b -> b.withProjectDir(this.projectDir.toFile()));
+    final Project subproject = IndraTesting.project(b -> b.withParent(base));
+    subproject.getPlugins().apply(PLUGIN);
+
+    assertTrue(subproject.getExtensions().getByType(IndraGitExtension.class).isPresent());
+  }
+
+  @Test
+  void testRepositoryDetectedThroughSubmodule() throws IOException, GitAPIException {
+    final Path mainProject = this.projectDir.resolve("main");
+    final Path submodule = this.projectDir.resolve("submodule");
+    Files.createDirectories(mainProject);
+    Files.createDirectories(submodule);
+
+    IndraTesting.exec(mainProject, "git", "init");
+    IndraTesting.exec(submodule, "git", "init");
+    Git.open(submodule.toFile()).commit()
+    .setAllowEmpty(true)
+    .setMessage("initial commit")
+    .setAuthor(COMMITTER)
+    .setCommitter(COMMITTER)
+    .call();
+
+    IndraTesting.exec(mainProject, "git", "submodule", "add", "../submodule", "child");
+
+    final Path referencedProjectDir = mainProject.resolve("child");
+    final Project inSubmodule = IndraTesting.project(b -> b.withProjectDir(referencedProjectDir.toFile()));
+    inSubmodule.getPluginManager().apply(PLUGIN);
+
+    assertTrue(inSubmodule.getExtensions().getByType(IndraGitExtension.class).isPresent());
+  }
+
+  @Test
+  void testHeadTagNullWhenNotCheckedOutToTag() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+    assertNull(extension.headTag());
+
+    Files.write(this.projectDir.resolve("test.properties"), Collections.singletonList("boink"), StandardCharsets.UTF_8);
+
+    extension.git().commit()
+      .setAll(true)
+      .setMessage("Initial commit")
+      .setCommitter(COMMITTER)
+      .call();
+
+    extension.git().tag()
+      .setName("v1")
+      .setAnnotated(false)
+      .call();
+
+    // Then move past the tag
+
+    Files.write(this.projectDir.resolve("test.properties"), Collections.singletonList("blah"), StandardCharsets.UTF_8);
+
+    extension.git().commit()
+      .setAll(true)
+      .setMessage("stage 2")
+      .setCommitter(COMMITTER)
+      .call();
+
+    assertNull(extension.headTag());
+  }
+
+  @Test
+  void testHeadTag() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+
+    Files.write(this.projectDir.resolve("test.properties"), Collections.singletonList("boink"), StandardCharsets.UTF_8);
+
+    extension.git().commit()
+      .setAll(true)
+      .setMessage("Initial commit")
+      .setCommitter(COMMITTER)
+      .call();
+
+    extension.git().tag()
+      .setName("v1")
+      .setAnnotated(false)
+      .call();
+
+    assertEquals("v1", Repository.shortenRefName(extension.headTag().getName()));
+  }
+
+  @Test
+  void testAnnotatedHeadTag() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+    Files.write(this.projectDir.resolve("test.properties"), Collections.singletonList("boink"), StandardCharsets.UTF_8);
+
+    extension.git().commit()
+      .setAll(true)
+      .setMessage("Initial commit")
+      .setCommitter(COMMITTER)
+      .call();
+
+    extension.git().tag()
+      .setName("v1")
+      .setMessage("Release v1")
+      .setTagger(COMMITTER)
+      .setAnnotated(true)
+      .call();
+
+    assertEquals("v1", Repository.shortenRefName(extension.headTag().getName()));
+  }
+
+  @Test
+  void testBranchOnInitialCommit() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+    assertEquals(DEFAULT_BRANCH, extension.branchName());
+  }
+
+  @Test
+  void testBranch() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+    Files.write(this.projectDir.resolve("test.properties"), Collections.singletonList("boink"), StandardCharsets.UTF_8);
+
+    extension.git().commit()
+      .setAll(true)
+      .setMessage("Initial commit")
+      .setCommitter(COMMITTER)
+      .call();
+
+    assertEquals(DEFAULT_BRANCH, extension.branchName());
+  }
+
+  @Test
+  void testBranchOnDetachedHead() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+    Files.write(this.projectDir.resolve("test.properties"), Collections.singletonList("boink"), StandardCharsets.UTF_8);
+
+    final RevCommit commit = extension.git().commit()
+      .setAll(true)
+      .setMessage("Initial commit")
+      .setCommitter(COMMITTER)
+      .call();
+
+    extension.git().checkout().setName(commit.name()).call();
+    assertNull(extension.branchName());
+  }
+
+  @Test
+  void testCommit() throws IOException, GitAPIException {
+    final IndraGitExtension extension = this.createExtensionAndRepo();
+    Files.write(this.projectDir.resolve("test.properties"), Collections.singletonList("boink"), StandardCharsets.UTF_8);
+
+    final RevCommit commit = extension.git().commit()
+      .setAll(true)
+      .setMessage("Initial commit")
+      .setCommitter(COMMITTER)
+      .call();
+
+    assertEquals(commit.getName(), extension.commit().getName());
+  }
+
+  private Project createProject() {
+    final Project project = IndraTesting.project(p -> p.withProjectDir(this.projectDir.toFile()));
+    project.getPluginManager().apply(PLUGIN);
+    return project;
+  }
+
+  private IndraGitExtension createExtension() {
+    return this.createProject().getExtensions().getByType(IndraGitExtension.class);
+  }
+
+  private IndraGitExtension createExtensionAndRepo() throws IOException, GitAPIException {
+    initRepo(this.projectDir);
+    return this.createExtension();
+  }
+
+  static Git initRepo(final Path repoDir) throws IOException, GitAPIException {
+    Files.createDirectories(repoDir);
+    final Git repo = Git.init()
+      .setDirectory(repoDir.toFile())
+      .setInitialBranch(DEFAULT_BRANCH)
+      .call();
+
+    return repo;
   }
 }
--- indra-testlib/src/main/java/net/kyori/indra/test/IndraTesting.java
+++ indra-testlib/src/main/java/net/kyori/indra/test/IndraTesting.java
@@ -23,10 +23,18 @@
  */
 package net.kyori.indra.test;
 
-import java.util.function.Consumer;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.function.Consumer;
 
 public class IndraTesting {
   public static Project project() {
@@ -39,5 +47,39 @@ public class IndraTesting {
       consumer.accept(builder);
     }
     return builder.build();
+  }
+
+  public static String exec(final Path workingDir, final String... cli) throws IOException {
+      final StringWriter error = new StringWriter();
+      final StringWriter output = new StringWriter();
+      final Process process = new ProcessBuilder(cli)
+          .directory(workingDir.toFile())
+          .redirectError(ProcessBuilder.Redirect.PIPE)
+          .redirectOutput(ProcessBuilder.Redirect.PIPE)
+          .start();
+
+      try (final InputStreamReader stdout = new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8);
+          final InputStreamReader stderr = new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8)) {
+          int read;
+          final char[] data = new char[8192];
+          while (process.isAlive()) {
+              if ((read = stdout.read(data)) != -1) {
+                  output.write(data, 0, read);
+              }
+              if ((read = stderr.read(data)) != -1) {
+                  error.write(data, 0, read);
+              }
+          }
+      }
+
+      if (process.exitValue() != 0) {
+          System.err.println("====== Standard error =====");
+          System.err.println(error.getBuffer());
+          System.err.println("====== Standard output =====");
+          System.err.println(output.getBuffer());
+          fail("Process " + String.join(" ", cli) + " exited with code " + process.exitValue());
+      }
+
+      return output.getBuffer().toString();
   }
 }
