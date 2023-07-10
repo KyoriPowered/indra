199a4d4bf0b9248ff55545d7a2039119536aae4b
Tim203 <mctim203@gmail.com>
2023-07-10 19:19:42 +0200

    Update tests

--- indra-common/src/test/java/net/kyori/indra/util/VersioningTest.java
+++ indra-common/src/test/java/net/kyori/indra/util/VersioningTest.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of indra, licensed under the MIT License.
  *
- * Copyright (c) 2020-2022 KyoriPowered
+ * Copyright (c) 2020-2023 KyoriPowered
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,28 +23,16 @@
  */
 package net.kyori.indra.util;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Stream;
-import net.kyori.indra.git.GitPlugin;
 import net.kyori.indra.test.IndraTesting;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.gradle.api.JavaVersion;
 import org.gradle.api.Project;
-import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class VersioningTest {
   @Test
